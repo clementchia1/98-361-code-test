64af1342606c1cff7ac220cb5f78970e5198de05
lemon <chiachingman1@gmail.com>
2021-04-26 15:18:08 +1000

    Add project files.

---
+++ 98-361 code test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _98_361_code_test
+{
+    class Program
+    {
+        public static int i = 1;
+        static void Main(string[] args)
+        {
+            foreach (var item in Ranks(true))
+            {
+                Console.WriteLine("Ranks:" + item);
+            }
+
+            Console.WriteLine("i%2:"+ i % 2);
+            
+        }
+
+        static IEnumerable<string> Ranks(bool a)
+        {
+            yield return "John";
+
+            if (a)
+            {
+                yield break;
+                
+            }
+            yield return "Chris";
+        }
+    }
+}
